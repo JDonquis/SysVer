daf80a14439c843a0115df4fc9ffbed58094c907
Donquis <juandonquis07@gmail.com>
2023-04-28 17:17:57 -0400

    update seeder user

--- laravel-api/database/seeders/UserSeeder.php
+++ laravel-api/database/seeders/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
     {
             $fields = [
 
-            ['name' => 'admin', 'last_name' => 'admin', 'ci' => '1234', 'password' => Hash::make('12345678'), 'birth_date' => '2002-09-07', 'age' => 20, 'sex' => 'M', 'blood_type_id' => 1, 'weight' => 70, 'height' => 175, 'address' => 'Some Place', 'phone_number' => '00000000000', 'collaboration' => 'none'],
+            ['name' => 'admin', 'last_name' => 'admin', 'ci' => '12345678', 'password' => Hash::make('12345678'), 'birth_date' => '2002-09-07', 'age' => 20, 'sex' => 'M', 'blood_type_id' => 1, 'weight' => 70, 'height' => 175, 'address' => 'Some Place', 'phone_number' => '00000000000', 'collaboration' => 'none'],
 
 
          ];   
