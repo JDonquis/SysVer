73150afcec6d9bba08b54e259767a61782ff949c
Donquis <juandonquis07@gmail.com>
2023-04-28 10:36:51 -0400

    add new fields to users table

--- laravel-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,18 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('last_name');
+            $table->string('ci')->unique();
             $table->string('password');
+            $table->date('birth_date');
+            $table->integer('age');
+            $table->string('sex');
+            $table->foreignId("blood_type_id")->constrained()->onDelete("restrict")->onUpdate("cascade");
+            $table->integer('weight');
+            $table->integer('height');
+            $table->string('address',100);
+            $table->string('phone_number',11);
+            $table->string('collaboration',100);           
             $table->rememberToken();
             $table->timestamps();
         });
