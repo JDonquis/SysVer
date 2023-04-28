d7b16f341e7f3dddfd581d32528a1461378d7c19
Donquis <juandonquis07@gmail.com>
2023-04-28 10:38:00 -0400

    add blood_types,areas,user_areas,shifts,assistances tables

---
+++ laravel-api/database/migrations/2023_04_28_052345_create_assistances_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssistancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assistances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_area_id")->constrained()->onDelete("restrict")->onUpdate("cascade");
+            $table->foreignId("shift_id")->constrained()->onDelete("restrict")->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assistances');
+    }
+}
