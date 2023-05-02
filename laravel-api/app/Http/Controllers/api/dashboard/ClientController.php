50b513aef3fb9b61dc9a95f0c6508935e83d7ec5
Donquis <juandonquis07@gmail.com>
2023-05-01 23:05:36 -0400

    give blood_types

--- laravel-api/app/Http/Controllers/api/dashboard/ClientController.php
+++ laravel-api/app/Http/Controllers/api/dashboard/ClientController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\api\dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\Area;
+use App\Models\BloodType;
 use App\Models\Client;
+use DB;
+use Exception;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Exception;
-use DB;
 
 class ClientController extends Controller
 {
@@ -16,7 +17,8 @@ class ClientController extends Controller
     {
         $clients = Client::with('blood_types','areas')->get();
         $areas = Area::all();
-        return response(["clients" => $clients, 'all_areas_db' => $areas], Response::HTTP_OK);
+        $blood_types = BloodType::all();
+        return response(["clients" => $clients, 'all_areas_db' => $areas, 'blood_types' => $blood_types], Response::HTTP_OK);
     }
 
     public function store(Request $request)
@@ -48,54 +50,42 @@ class ClientController extends Controller
             DB::rollback();
 
             if($e->getCode() == '23000')
-                return response(["Message" => 'Cedula ya encontrada en la base de datos'], Response::HTTP_BAD_REQUEST);    
+                return response(["Message" => 'No se pudo crear el cliente, verifique los datos'], Response::HTTP_BAD_REQUEST);    
             
             return response(["Message" => 'No se pudo crear el cliente'], Response::HTTP_BAD_REQUEST);
         }   
-    
-    
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        return $request;
+
+        $client = new Client;
+        
+       DB::beginTransaction();
+
+        try {
+
+            DB::table('clients')
+            ->where('id', 3)
+            ->update(['title' => "Updated Title"]);
+
+            foreach ($request->areas as $area){ DB::table('client_areas')->insert(['client_id' => $client_id, 'area_id' => $area]); }
+                
+            DB::commit();
+
+            return response(["Message" => 'Cliente creado exitosamente'], Response::HTTP_OK);
+
+        }catch (Exception $e) {
+            DB::rollback();
+
+            if($e->getCode() == '23000')
+                return response(["Message" => 'No se pudo crear el cliente, verifique los datos'], Response::HTTP_BAD_REQUEST);    
+            
+            return response(["Message" => 'No se pudo crear el cliente'], Response::HTTP_BAD_REQUEST);
+        }   
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         DB::beginTransaction();
@@ -115,7 +105,5 @@ class ClientController extends Controller
             
             return response(["Message" => 'Cliente no encontrado'], Response::HTTP_BAD_REQUEST);
         }   
-    
-        
     }
 }
