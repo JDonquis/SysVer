358a5fcbc9c0f373b058f24a5e5091edea27d00e
Donquis <juandonquis07@gmail.com>
2023-05-17 18:36:57 -0400

    give shifts

--- laravel-api/app/Http/Controllers/api/dashboard/AreasController.php
+++ laravel-api/app/Http/Controllers/api/dashboard/AreasController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\api\dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\Area;
+use App\Models\Shift;
+use DB;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use DB;
 
 class AreasController extends Controller
 {
@@ -18,8 +19,9 @@ class AreasController extends Controller
     public function index()
     {
         $areas = Area::with('schedule.shift_start','schedule.shift_end','schedule.days')->get();
+        $shifts = Shift::all();
 
-        return response(["areas" => $areas], Response::HTTP_OK);
+        return response(["areas" => $areas,'shifts' => $shifts], Response::HTTP_OK);
     }
 
     /**
@@ -29,27 +31,45 @@ class AreasController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {   
+        // return $request;
+
         DB::beginTransaction();
         try {
                 $area_id = DB::table('areas')->insertGetId(['name' => $request->name, 'type_area_id' => $request->type_area_id ] );
 
+                
+
+                // No gratis
                 if($request->type_area_id == 2)
-                {
                     DB::table('area_chargeds')->insert(['area_id' => $area_id, 'name' => $request->name, 'price' => $request->price ] );    
+                
+                foreach ($request->schedules as $schedule) {
+
+                    $s = DB::table('schedules')->insertGetId(['start_shift_id' => $schedule['start_shift_id'], 'end_shift_id' => $schedule['end_shift_id'], 'area_id' => $area_id ] );
+
+                    foreach ($schedule['days'] as $day)
+                    {   
+
+                        DB::table('schedule_days')->insert(['day_id' => $day, 'schedule_id' => $s ] );
+                        
+                    }
+
                 }
+                                
+            $area = Area::where('id',$area_id)->with('schedule.shift_start','schedule.shift_end','schedule.days')->first();    
 
             DB::commit();
 
-            return response(["Message" => 'Cliente creado exitosamente', "client" => $client_created], Response::HTTP_OK);
+            return response(["Message" => 'Area creada exitosamente', "area" => $area], Response::HTTP_OK);
 
         }catch (Exception $e) {
             DB::rollback();
 
             if($e->getCode() == '23000')
-                return response(["Message" => 'No se pudo crear el cliente, verifique los datos', 'ErrorMessage' => $e->getMessage()], Response::HTTP_BAD_REQUEST);    
+                return response(["Message" => 'No se pudo crear el area, verifique los datos', 'ErrorMessage' => $e->getMessage()], Response::HTTP_BAD_REQUEST);    
             
-            return response(["Message" => 'No se pudo crear el cliente','ErrorMessage' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+            return response(["Message" => 'No se pudo crear el area','ErrorMessage' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
         }
         //No gratis 
 
