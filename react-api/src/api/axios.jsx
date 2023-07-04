8537d03b1731f6a0b0e6fa2c6f52e49b9f302bcb
Donquis <juandonquis07@gmail.com>
2023-07-04 11:37:40 -0400

    change api axios

--- react-api/src/api/axios.jsx
+++ react-api/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://sysver.local/api",
   withCredentials: true,
   browserBaseURL: 'http://'
 })
